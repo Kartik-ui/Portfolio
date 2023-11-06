f6880360b348689be2b6d2633a9d216083bc883a
Kartik-ui <imkartik1311@gmail.com>
2023-11-06 23:36:04 +0530

    Initial Commit

---
+++ backend_sanity/schemas/index.ts
@@ -0,0 +1,19 @@
+import testimonials from './testimonials'
+import works from './works'
+import workExperience from './workExperience'
+import skills from './skills'
+import experiences from './experiences'
+import contact from './contact'
+import brands from './brands'
+import abouts from './abouts'
+
+export const schemaTypes = [
+  works,
+  testimonials,
+  brands,
+  abouts,
+  skills,
+  workExperience,
+  experiences,
+  contact,
+]
